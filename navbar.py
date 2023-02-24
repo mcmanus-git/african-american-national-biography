db67e62f220b82efe0360338dddba5ae967ddaf5
Michael M <melancholic_maclean@yahoo.com>
2023-02-23 23:16:43 -0500

    update navbar with image background

--- navbar.py
+++ navbar.py
@@ -3,59 +3,73 @@ import dash_bootstrap_components as dbc
 
 
 def create_navbar():
-    navbar = dbc.NavbarSimple(
-        children=[
-            dbc.NavItem(
-                dbc.NavLink(
-                    [
-                        html.I(className="fa-brands fa-github"),  # Font Awesome Icon
-                        " "  # Text beside icon
-                    ],
-                    href="https://github.com/mcmanus-git",
-                    target="_blank"
-                )
+    navbar = html.Div(
+        [
+            dbc.NavbarSimple(
+                # class_name='navbar bg-transparent',
+                children=[
+                    dbc.NavItem(
+                        dbc.NavLink(
+                            [
+                                html.I(className="fa-brands fa-github"),  # Font Awesome Icon
+                                " "  # Text beside icon
+                            ],
+                            href="https://github.com/mcmanus-git",
+                            target="_blank"
+                        )
 
-            ),
-            dbc.NavItem(
-                dbc.NavLink(
-                    [
-                        html.I(className="fa-brands fa-medium"),  # Font Awesome Icon
-                        " "  # Text beside icon
-                    ],
-                    href="https://medium.com/@mcmanus_data_works",
-                    target="_blank"
-                )
+                    ),
+                    dbc.NavItem(
+                        dbc.NavLink(
+                            [
+                                html.I(className="fa-brands fa-medium"),  # Font Awesome Icon
+                                " "  # Text beside icon
+                            ],
+                            href="https://medium.com/@mcmanus_data_works",
+                            target="_blank"
+                        )
 
-            ),
-            dbc.NavItem(
-                dbc.NavLink(
-                    [
-                        html.I(className="fa-brands fa-linkedin"),  # Font Awesome Icon
-                        " "  # Text beside icon
-                    ],
-                    href="https://www.linkedin.com/in/michael-mcmanus/",
-                    target="_blank"
-                )
+                    ),
+                    dbc.NavItem(
+                        dbc.NavLink(
+                            [
+                                html.I(className="fa-brands fa-linkedin"),  # Font Awesome Icon
+                                " "  # Text beside icon
+                            ],
+                            href="https://www.linkedin.com/in/michael-mcmanus/",
+                            target="_blank"
+                        )
 
-            ),
-            dbc.DropdownMenu(
-                nav=True,
-                in_navbar=True,
-                label="Menu",
-                align_end=True,
-                children=[  # Add as many menu items as you need
-                    dbc.DropdownMenuItem("Home", href='/'),
-                    dbc.DropdownMenuItem(divider=True),
-                    dbc.DropdownMenuItem("About", href='/about'),
-                    dbc.DropdownMenuItem("Contact Us", href='/contact-us'),
+                    ),
+                    dbc.DropdownMenu(
+                        nav=True,
+                        in_navbar=True,
+                        label="Menu",
+                        align_end=True,
+                        children=[  # Add as many menu items as you need
+                            dbc.DropdownMenuItem("Home", href='/'),
+                            dbc.DropdownMenuItem(divider=True),
+                            dbc.DropdownMenuItem("About", href='/about'),
+                            dbc.DropdownMenuItem("Contact Us", href='/contact-us'),
+                        ],
+                    ),
                 ],
-            ),
+                brand='African American National Biography',
+                brand_href="/",
+                # sticky="top",  # Uncomment if you want the navbar to always appear at the top on scroll.
+                # color="dark",  # Change this to change color of the navbar e.g. "primary", "secondary" etc.
+                dark=False,  # Change this to change color of text within the navbar (False for dark text)
+                style={
+                    'background-color': '#000000',
+                    'opacity': '0.75'
+                }
+            )
         ],
-        brand='Home',
-        brand_href="/",
-        # sticky="top",  # Uncomment if you want the navbar to always appear at the top on scroll.
-        color="dark",  # Change this to change color of the navbar e.g. "primary", "secondary" etc.
-        dark=True,  # Change this to change color of text within the navbar (False for dark text)
+        style={
+            'background-image': 'url(/assets/library-of-congress-WzPxmB_tRlw-unsplash.jpg)',
+            'background-size': 'cover',
+            'height': '50vh',
+        }
     )
 
     return navbar
