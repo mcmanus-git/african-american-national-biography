e44ac9961ad67d73f846a9b1f8376996a59034ee
Michael M <melancholic_maclean@yahoo.com>
2023-02-24 22:48:09 -0500

    reformat code pep

--- visualizations/vis_education.py
+++ visualizations/vis_education.py
@@ -4,7 +4,17 @@ import plotly.express as px
 def create_total_education_vis(df):
 
     total_ed_vis = px.bar(
-        df['education_literacy_bool'].value_counts().reset_index().rename({'index': "Literate or Educated", 'education_literacy_bool': 'Number of People'}, axis=1),
+        (df['education_literacy_bool']
+         .value_counts()
+         .reset_index()
+         .rename(
+            {
+                'index': "Literate or Educated",
+                'education_literacy_bool': 'Number of People'
+            },
+            axis=1
+        )
+        ),
         x='Literate or Educated',
         y='Number of People',
     )
