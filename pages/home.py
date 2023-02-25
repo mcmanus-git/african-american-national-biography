05fc36ac8680281f39f36983039646b0d1b81f71
Michael M <melancholic_maclean@yahoo.com>
2023-02-25 00:18:15 -0500

    add explanation paragraph

--- pages/home.py
+++ pages/home.py
@@ -27,9 +27,21 @@ def layout():
         [
             html.H1(
                 [
-                    "African American National Biography Data Exploration"
-                ]
+                    "African American National Biography"
+                ],
+                style={
+                    'text-align': 'center'
+                }
             ),
+            html.H2(
+                [
+                    "Data Exploration"
+                ],
+                style={
+                    'text-align': 'center'
+                }
+            ),
+            dcc.Markdown("""This dashboard is an exploration of a subset of data taken from the [African American National Biography](https://hutchinscenter.fas.harvard.edu/AANB) of over 1,300 people who lived prior to ratification of The 13th Amendment of the United States Constitution between the years 1508-1865. For more information about the dataset please see our [About Page](/about)."""),
             html.Div(
                 [
                     dcc.Graph(figure=life_expectancy_vis),
