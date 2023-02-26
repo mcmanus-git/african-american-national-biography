f60771fad64413ad9d3ebca9688b4a747a9728b4
Michael McManus <melancholic_maclean@yahoo.com>
2023-02-26 02:00:30 -0500

    clean up commented code

--- pages/about.py
+++ pages/about.py
@@ -1,24 +1,6 @@
 from dash import html, register_page, dcc  #, callback # If you need callbacks, import it here.
 import dash_bootstrap_components as dbc
 
-# register_page(
-#     __name__,
-#     name='About',
-#     path='/about',
-#     title="About",
-#     description="AANB App About Page.",
-# )
-#
-#
-# def layout():
-#     layout = html.Div([
-#         html.H1(
-#             [
-#                 "About Page"
-#             ]
-#         )
-#     ])
-#     return layout
 
 register_page(
     __name__,
