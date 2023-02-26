3bca02e628a87fa70d9e011820f4b31e72d0312a
Michael M <melancholic_maclean@yahoo.com>
2023-02-25 23:56:33 -0500

    add bios on dropdown select

--- pages/biographies.py
+++ pages/biographies.py
@@ -9,27 +9,69 @@ register_page(
     description='AANB App Biography Page'
 )
 
+DF = pd.read_pickle('data/aanb_data_clean.pkl')
+BIO_LIST = DF['full_name'].to_list()
 
-def bio_select(df):
-    bio_select_dropdown = dcc.Dropdown(
-        df['full_name'].to_list(),
-        id='bio-select-dropdown',
-        placeholder='Select a Biography'
+
+def bio_template(df):
+
+    dob = df['dob'].fillna('Unknown').values[0]
+    dod = df['dod'].fillna('Unknown').values[0]
+    age = df['age_at_death'].round(0).fillna('Unknown').values[0]
+    enslaved = df['ever_enslaved_bool'].fillna('Unknown').values[0]
+    thirteenth = df['free_before_13_bool'].fillna('Unknown').values[0]
+    rr = df['runaways_and_rebels_bool'].fillna('Unknown').values[0]
+    ed = df['education_literacy_bool'].fillna('Unknown').values[0]
+    occ = ", ".join([v[0].strip() for v in df[[col for col in df.columns if 'Occupation ' in col]].T.dropna().values])
+    religion = df['religion_clean'].fillna('Unknown').values[0]
+    notes = df['Notes'].fillna('No Records').values[0]
+
+    bio = dcc.Markdown(f"""## {df['full_name'].values[0]}  
+<h5>Date of Birth:</h5> {dob}
+<h5>Date of Death:</h5>  {dod}  
+<h5>Age:</h5>  {age}  
+<h5>Ever Enslaved:</h5>  {enslaved}  
+<h5>Freed Before 13th Amendment:</h5>  {thirteenth}  
+<h5>Runaway or Rebel:</h5>  {rr}  
+<h5>Educated and/or Literate:</h5>  {ed}  
+<h5>Occupations:</h5>  {occ}  
+<h5>Religion:</h5>  {religion}  
+<h4>Notes:</h4>  
+{notes}  
+""",
+                       dangerously_allow_html=True
+                       )
+
+    bio_container = html.Div(
+        [
+            bio
+        ],
+        style={
+            'padding': '5% 5% 5% 5%'
+        }
     )
 
-    return bio_select_dropdown
+    return bio_container
+
 
 @callback(
     Output('bio-output-container', 'children'),
     Input('bio-select-dropdown', 'value')
 )
 def update_output(value):
-    return f'You have selected {value}'
+
+    if value:
+        bios = DF[DF['full_name'] == value]
+
+        bio = bio_template(bios)
+
+        # return f'You have selected {value} and df returned {bios.shape} {bios["ever_enslaved_bool"].values[0]}'
+        return bio
+    else:
+        pass
+
 
 def layout():
-
-    df = pd.read_pickle('data/aanb_data_clean.pkl')
-    bios_list = df['full_name'].to_list()
 
     layout = html.Div(
         [
@@ -39,7 +81,7 @@ def layout():
                 ]
             ),
             dcc.Dropdown(
-                bios_list,
+                BIO_LIST,
                 id='bio-select-dropdown',
                 placeholder='Select a Biography'
             ),
