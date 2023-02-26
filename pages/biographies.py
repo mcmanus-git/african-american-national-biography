4874cb9765efa36046921aa7260a8230b2f88b17
Michael McManus <melancholic_maclean@yahoo.com>
2023-02-26 01:09:33 -0500

    update biographies template

--- pages/biographies.py
+++ pages/biographies.py
@@ -13,42 +13,48 @@ DF = pd.read_pickle('data/aanb_data_clean.pkl')
 BIO_LIST = DF['full_name'].to_list()
 
 
-def bio_template(df):
+def bio_template(df, val):
 
+    bio_items_style = {'padding': '2% 0% 0% 0%'}
+
+    name = val
     dob = df['dob'].fillna('Unknown').values[0]
     dod = df['dod'].fillna('Unknown').values[0]
     age = df['age_at_death'].round(0).fillna('Unknown').values[0]
     enslaved = df['ever_enslaved_bool'].fillna('Unknown').values[0]
+    free_meth = df['How was freedom attained'].fillna('Unknown').values[0]
     thirteenth = df['free_before_13_bool'].fillna('Unknown').values[0]
     rr = df['runaways_and_rebels_bool'].fillna('Unknown').values[0]
     ed = df['education_literacy_bool'].fillna('Unknown').values[0]
-    occ = ", ".join([v[0].strip() for v in df[[col for col in df.columns if 'Occupation ' in col]].T.dropna().values])
+    occupation_list = [v[0].strip() for v in df[[col for col in df.columns if 'Occupation ' in col]].T.dropna().values]
+    if len(occupation_list) == 0:
+        occ = 'Unknown'
+    else:
+        occ = ", ".join(occupation_list)
     religion = df['religion_clean'].fillna('Unknown').values[0]
     notes = df['Notes'].fillna('No Records').values[0]
 
-    bio = dcc.Markdown(f"""## {df['full_name'].values[0]}  
-<h5>Date of Birth:</h5> {dob}
-<h5>Date of Death:</h5>  {dod}  
-<h5>Age:</h5>  {age}  
-<h5>Ever Enslaved:</h5>  {enslaved}  
-<h5>Freed Before 13th Amendment:</h5>  {thirteenth}  
-<h5>Runaway or Rebel:</h5>  {rr}  
-<h5>Educated and/or Literate:</h5>  {ed}  
-<h5>Occupations:</h5>  {occ}  
-<h5>Religion:</h5>  {religion}  
-<h4>Notes:</h4>  
-{notes}  
-""",
-                       dangerously_allow_html=True
-                       )
-
-    bio_container = html.Div(
-        [
-            bio
-        ],
-        style={
-            'padding': '5% 5% 5% 5%'
-        }
+    bio_container = dcc.Loading(
+        html.Div(
+            [
+                html.H2(name, style={'text-align': 'center'}),
+                html.Div([html.H5('Date of Birth: '), dob], style=bio_items_style),
+                html.Div([html.H5('Date of Death: '), dod], style=bio_items_style),
+                html.Div([html.H5('Age: '), age], style=bio_items_style),
+                html.Div([html.H5('Ever Enslaved: '), enslaved], style=bio_items_style),
+                html.Div([html.H5('Freed Before 13th Amendment: '), thirteenth], style=bio_items_style),
+                html.Div([html.H5('Runaway or Rebel: '), rr], style=bio_items_style),
+                html.Div([html.H5('Educated and/or Literate: '), ed], style=bio_items_style),
+                html.Div([html.H5('Occupations: '), occ], style=bio_items_style),
+                html.Div([html.H5('Religion: '), religion], style=bio_items_style),
+                html.Div([html.H5('Was Freedom Attained: '), free_meth], style=bio_items_style),
+                html.Div([html.H4('Notes: '), notes], style=bio_items_style),
+                # html.Div([html.H4('Notes: '), notes], style=bio_items_style),
+            ],
+            style={
+                'padding': '5% 5% 5% 5%'
+            }
+        )
     )
 
     return bio_container
@@ -62,8 +68,7 @@ def update_output(value):
 
     if value:
         bios = DF[DF['full_name'] == value]
-
-        bio = bio_template(bios)
+        bio = bio_template(bios, value)
 
         # return f'You have selected {value} and df returned {bios.shape} {bios["ever_enslaved_bool"].values[0]}'
         return bio
